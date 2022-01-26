f56180e79d9fa59971e9122b68195dc949843881
Nick Kushkbaghi <nick.kushkbaghi@gmail.com>
2022-01-26 20:28:37 +0100

    first commit

---
+++ wwwroot/js/index.js
@@ -0,0 +1,8 @@
+﻿"use strict";
+// Set current page as the active page
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll("aside a").forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
+});
